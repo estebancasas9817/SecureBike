4f849740fd112d9fa7c3989997147042a59a3427
Gabriel-Forero <69248583+Gabriel-Forero@users.noreply.github.com>
2021-03-01 13:00:08 -0500

    Pantalla home

--- app/src/main/java/com/company/securebike/Home.java
+++ app/src/main/java/com/company/securebike/Home.java
@@ -2,7 +2,10 @@ package com.company.securebike;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button buttonConf = (Button)findViewById(R.id.b1);
+        Button buttonCont = (Button)findViewById(R.id.b3);
+
+        buttonConf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWeb = new Intent(v.getContext(),Configuracion.class);
+                startActivity(intentWeb);
+            }
+        });
+
     }
 }
