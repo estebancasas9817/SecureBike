abcfdc4ff94933c165eea982ff51ec4ae83be49f
Gabriel-Forero <69248583+Gabriel-Forero@users.noreply.github.com>
2021-03-01 12:26:59 -0500

    Boton atras

--- app/src/main/java/com/company/securebike/Configuracion.java
+++ app/src/main/java/com/company/securebike/Configuracion.java
@@ -2,7 +2,10 @@ package com.company.securebike;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Configuracion extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Configuracion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
+
+        Button buttonAtras = (Button)findViewById(R.id.button1);
+        buttonAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Configuracion.this,Home.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
